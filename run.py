ab82f0c332cb5f16ddb6367d88d2338d22887d8b
root <root@s17894922.onlinehome-server.info>
2014-10-22 11:46:07 +0200

    added utc timestamp

--- run.py
+++ run.py
@@ -3,14 +3,14 @@ import datetime
 
 r = praw.Reddit(user_agent='get_me_reddit')
 i = 0
-submissions = r.get_subreddit('bitcoin').get_new(limit=2000)
+submissions = r.get_subreddit('bitcoin').get_new(limit=10)
 for s in submissions:
   i = i + 1
   print str(s.ups) +" - "+ s.title
   flat_comments = praw.helpers.flatten_tree(s.comments)
   for c in flat_comments:
     if isinstance(c, praw.objects.Comment):
-      print str(c.created_utc) + " " + datetime.datetime.fromtimestamp(c.created_utc).strftime('%Y-%m-%d %H:%M:%S')
+      print str(c.created_utc) + " " + datetime.datetime.utcfromtimestamp(c.created_utc).strftime('%Y-%m-%d %H:%M:%S')
       print "---" + str(c.ups) + " - " + c.body
 
 print i
