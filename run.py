ecdc89a46b227812ad44df238b62c19e97b29596
gregor <gpribb@gmail.com>
2014-10-23 21:14:45 +0200

    bypass reddit limit of 1000 responses

--- run.py
+++ run.py
@@ -26,52 +26,72 @@ def connectToDb():
 
     return db
 
-def getSubmissions():
+def getSubmissions(start):
     r = praw.Reddit(user_agent='get_me_reddit')
-    submissions = r.get_subreddit('bitcoin').get_new(limit=10)
+    # pro Tag eine Abfrage - quelle: http://www.reddit.com/r/redditdev/comments/2j6inf/how_to_download_submissions_of_a_specified_period/
+    time_search_param = 'timestamp:' + str(start - 86600) + '..' + str(start)
+    print("von", datetime.datetime.utcfromtimestamp(start-86600).strftime('%Y-%m-%d %H:%M:%S'), "bis", datetime.datetime.utcfromtimestamp(start).strftime('%Y-%m-%d %H:%M:%S'))
+    submissions = r.search(time_search_param, sort='new', subreddit='bitcoin', syntax='cloudsearch')
     return submissions
 
 def main():
     db = connectToDb()
     # you must create a Cursor object. It will let you execute all the queries you need
     cur = db.cursor()
-    submissions = getSubmissions()
     i = 0
-    for s in submissions:
-          i = i + 1
-          print(datetime.datetime.now(), "-------------------------")
-          date = datetime.datetime.utcfromtimestamp(s.created_utc).strftime('%Y-%m-%d %H:%M:%S')
-          print (str(s.created_utc), date, str(s.id))
-          print (str(s.ups), "-", s.title.encode('ascii', 'ignore'))
-          sql = "INSERT INTO `submissions`(`id`, `timestamp`, `date`, `title`, `body`, `upvotes`, `eng_pos`, `eng_neg`, `score`) \
-          VALUES ('%s', '%i', '%s', '%s', '%s', '%i', '%i', '%i', '%i' )" % \
-          (str(s.id),int(s.created_utc),date,s.title.replace("'","''"),s.selftext.replace("'","''"),s.ups,0,0,0)
-          cur.execute(sql)
-          db.commit()
-          # problem ist die Struktur von reddit, was hier umgangen wird, indem alles platt geklopft wird
-          for attempt in range(10):
-              try:
-                  flat_comments = praw.helpers.flatten_tree(s.comments)
-              except Exception as e:
-                  warning(e)
+    start = 1413581567
+    while 1:
+        # probier 10x reddit zu erreichen
+        for attempt in range(10):
+            try:
+                submissions = getSubmissions(start)
+            except Exception as e:
+                warning(e)
+                time.sleep(4)
+            else:
+                break
 
-          for c in flat_comments:
-              #nochmal nachlesen: Problem "morecomments"
-              if isinstance(c, praw.objects.Comment):
-                  date = datetime.datetime.utcfromtimestamp(c.created_utc).strftime('%Y-%m-%d %H:%M:%S')
-                  print(str(c.created_utc), date, str(c.id))
-                  print ("---", str(c.ups), c.body.encode('ascii', 'ignore'))
-                  sql = "INSERT INTO `submissions`(`id`, `timestamp`, `date`, `title`, `body`, `upvotes`, `eng_pos`, `eng_neg`, `score`) \
-                  VALUES ('%s', '%i', '%s', '%s', '%s', '%i', '%i', '%i', '%i' )" % \
-                  (str(c.id),int(c.created_utc),date,'',c.body.replace("'","''"),c.ups,0,0,0)
-                  cur.execute(sql)
-                  db.commit()
+        for s in submissions:
+            i = i + 1
+            print(datetime.datetime.now(), "-------------------------")
+            date = datetime.datetime.utcfromtimestamp(s.created_utc).strftime('%Y-%m-%d %H:%M:%S')
+            print (str(s.created_utc), date, str(s.id))
+            print (str(s.ups), "-", s.title.encode('ascii', 'ignore'))
+            sql = "INSERT INTO `submissions`(`id`, `timestamp`, `date`, `title`, `body`, `upvotes`, `processed_on`, `eng_pos`, `eng_neg`, `score`) \
+            VALUES ('%s', '%i', '%s', '%s', '%s', '%i', '%s', '%i', '%i', '%i' )" % \
+            (str(s.id),int(s.created_utc),date,s.title.replace("'","''"),s.selftext.replace("'","''"),s.ups,'',0,0,0)
+            cur.execute(sql)
+            db.commit()
+            # probier 10x reddit zu erreichen
+            for attempt in range(10):
+                try:
+                    # problem ist die Struktur von reddit, was hier umgangen wird, indem alles platt geklopft wird
+                    flat_comments = praw.helpers.flatten_tree(s.comments)
+                except Exception as e:
+                    warning(e)
+                    time.sleep(4)
+                else:
+                    break
+
+            for c in flat_comments:
+                #nochmal nachlesen: Problem "morecomments"
+                if isinstance(c, praw.objects.Comment):
+                    date = datetime.datetime.utcfromtimestamp(c.created_utc).strftime('%Y-%m-%d %H:%M:%S')
+                    print(str(c.created_utc), date, str(c.id))
+                    print ("---", str(c.ups), c.body.encode('ascii', 'ignore'))
+                    sql = "INSERT INTO `submissions`(`id`, `timestamp`, `date`, `title`, `body`, `upvotes`, `processed_on`, `eng_pos`, `eng_neg`, `score`) \
+                    VALUES ('%s', '%i', '%s', '%s', '%s', '%i', '%s', '%i', '%i', '%i' )" % \
+                    (str(c.id),int(c.created_utc),date,'',c.body.replace("'","''"),c.ups,'',0,0,0)
+                    cur.execute(sql)
+                    db.commit()
+        # einen Tag zurück, bitte!
+        start = start - 86601
+        print("Anzahl submissions:", i)
 
 
 
 
 
-    print(i)
     db.close()
 
 if __name__ == '__main__':
