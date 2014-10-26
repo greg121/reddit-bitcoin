d22d758902c4f7e4e68980c1f3f204cfcf009ae2
gregor <gpribb@gmail.com>
2014-10-26 11:56:37 +0100

    log file is generated while running

--- run.py
+++ run.py
@@ -9,9 +9,6 @@ import ConfigParser
 import time
 import sys
 
-def warning(*objs):
-    print(datetime.datetime.now(), "WARNING: ", *objs, file=sys.stderr)
-
 def connectToDb():
     # config einlesen
     config = ConfigParser.RawConfigParser()
@@ -21,15 +18,15 @@ def connectToDb():
     db = MySQLdb.connect(host=config.get('mysql', 'host'),
                          user=config.get('mysql', 'user'),
                          passwd=config.get('mysql', 'passwd'),
-                         db=config.get('mysql', 'db_test'),
+                         db=config.get('mysql', 'db'),
                          charset="utf8")
 
     return db
 
-def addToDatabase(db, cur, id, created, date, link, title, body, ups, proc_on, pos, neg, score):
+def addToDatabase(db, cur, created, date, link, title, body, ups, proc_on, pos, neg, score):
     sql = "INSERT INTO `submissions`(`id`, `timestamp`, `date`, `link`, `title`, `body`, `upvotes`, `processed_on`, `eng_pos`, `eng_neg`, `score`) \
-    VALUES ('%s', '%i', '%s', '%s', '%s', '%s', '%i', '%s', '%i', '%i', '%i' )" % \
-    (id, created, date, link, title, body, ups, proc_on, pos, neg, score)
+    VALUES (NULL, '%s', '%s', '%s', '%s', '%s', '%i', '%s', '%i', '%i', '%i' )" % \
+    (created, date, link, title, body, ups, proc_on, pos, neg, score)
     cur.execute(sql)
     db.commit()
 
@@ -37,7 +34,7 @@ def getSubmissions(start):
     r = praw.Reddit(user_agent='get_me_reddit')
     # pro halben Tag (43200sec) eine Abfrage - quelle: http://www.reddit.com/r/redditdev/comments/2j6inf/how_to_download_submissions_of_a_specified_period/
     time_search_param = 'timestamp:' + str(start - 43200) + '..' + str(start)
-    print("von", datetime.datetime.utcfromtimestamp(start-43200).strftime('%Y-%m-%d %H:%M:%S'), "bis", datetime.datetime.utcfromtimestamp(start).strftime('%Y-%m-%d %H:%M:%S'))
+    writeLog("von " + datetime.datetime.utcfromtimestamp(start-43200).strftime('%Y-%m-%d %H:%M:%S') + " bis " + datetime.datetime.utcfromtimestamp(start).strftime('%Y-%m-%d %H:%M:%S'))
     submissions = r.search(time_search_param, sort='new', subreddit='bitcoin', syntax='cloudsearch', limit=1000)
     return submissions
 
@@ -48,19 +45,24 @@ def escapeSqlProblems(string):
     result = result.replace('\\',' ')
     return result
 
+def writeLog(string):
+    with open("log", "a") as myfile:
+        myfile.write(string + "\n")
+
 def main():
     db = connectToDb()
     # you must create a Cursor object. It will let you execute all the queries you need
     cur = db.cursor()
-    start = 1413581567
+    start = 1413581400
     while 1:
         i = 0
+        j = 0
         # probier 10x reddit zu erreichen
         for attempt in range(10):
             try:
                 submissions = getSubmissions(start)
             except Exception as e:
-                warning(e)
+                writeLog("ERROR:" + str(e))
                 time.sleep(4)
             else:
                 break
@@ -71,7 +73,7 @@ def main():
             date = datetime.datetime.utcfromtimestamp(s.created_utc).strftime('%Y-%m-%d %H:%M:%S')
             #print (str(s.created_utc), date, str(s.id))
             #print (str(s.ups), "-", s.title.encode('ascii', 'ignore'))
-            addToDatabase(db,cur,str(s.id),int(s.created_utc),date,s.permalink,escapeSqlProblems(s.title),escapeSqlProblems(s.selftext),s.ups,'',0,0,0)
+            addToDatabase(db,cur,int(s.created_utc),date,s.permalink,escapeSqlProblems(s.title),escapeSqlProblems(s.selftext),s.ups,'',0,0,0)
 
             # probier 10x reddit zu erreichen
             for attempt in range(10):
@@ -81,25 +83,23 @@ def main():
                     # problem ist die Struktur von reddit, was hier umgangen wird, indem alles platt geklopft wird
                     flat_comments = praw.helpers.flatten_tree(s.comments)
                 except Exception as e:
-                    warning(e)
+                    writeLog("ERROR:" + str(e))
                     time.sleep(4)
                 else:
                     break
 
             for c in flat_comments:
+                j = j + 1
                 #nochmal nachlesen: Problem "morecomments"
                 #if isinstance(c, praw.objects.Comment):
                 date = datetime.datetime.utcfromtimestamp(c.created_utc).strftime('%Y-%m-%d %H:%M:%S')
                 #print(str(c.created_utc), date, str(c.id))
                 #print ("---", str(c.ups), c.body.encode('ascii', 'ignore'))
-                addToDatabase(db,cur,str(c.id),int(c.created_utc),date,c.permalink,'',escapeSqlProblems(c.body),c.ups,'',0,0,0)
+                addToDatabase(db,cur,int(c.created_utc),date,c.permalink,'',escapeSqlProblems(c.body),c.ups,'',0,0,0)
         # rewind please! half a day = 43200sec
         start = start - 43201
-        print("Anzahl submissions:", i)
-
-
-
-
+        writeLog("number of submissions: " + str(i))
+        writeLog("number of comments: " + str(j))
 
     db.close()
 
