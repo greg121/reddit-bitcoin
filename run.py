24ab1405bced03820fc9775d59ab4f852bcd7f65
gregor <gpribb@gmail.com>
2014-10-27 12:01:36 +0100

    little changes

--- run.py
+++ run.py
@@ -2,26 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
-import MySQLdb
 import praw
 import datetime
-import ConfigParser
 import time
 import sys
-
-def connectToDb():
-    # config einlesen
-    config = ConfigParser.RawConfigParser()
-    config.read('config.ini')
-
-    # quelle: http://stackoverflow.com/a/622308/1018288
-    db = MySQLdb.connect(host=config.get('mysql', 'host'),
-                         user=config.get('mysql', 'user'),
-                         passwd=config.get('mysql', 'passwd'),
-                         db=config.get('mysql', 'db'),
-                         charset="utf8")
-
-    return db
+import MySQLdb
+import dbhelper
 
 def addToDatabase(db, cur, created, date, link, title, body, ups, proc_on, pos, neg, score):
     sql = "INSERT INTO `submissions`(`id`, `timestamp`, `date`, `link`, `title`, `body`, `upvotes`, `processed_on`, `eng_pos`, `eng_neg`, `score`) \
@@ -34,14 +20,15 @@ def getSubmissions(start):
     r = praw.Reddit(user_agent='get_me_reddit')
     # pro halben Tag (43200sec) eine Abfrage - quelle: http://www.reddit.com/r/redditdev/comments/2j6inf/how_to_download_submissions_of_a_specified_period/
     time_search_param = 'timestamp:' + str(start - 43200) + '..' + str(start)
-    writeLog("von " + datetime.datetime.utcfromtimestamp(start-43200).strftime('%Y-%m-%d %H:%M:%S') + " bis " + datetime.datetime.utcfromtimestamp(start).strftime('%Y-%m-%d %H:%M:%S'))
+    writeLog(str(datetime.now()) + ":    von " + datetime.datetime.utcfromtimestamp(start-43200).strftime('%Y-%m-%d %H:%M:%S') + " bis " + datetime.datetime.utcfromtimestamp(start).strftime('%Y-%m-%d %H:%M:%S'))
     submissions = r.search(time_search_param, sort='new', subreddit='bitcoin', syntax='cloudsearch', limit=1000)
     return submissions
 
 def escapeSqlProblems(string):
+    # standardmethode mal testen
+    #result = MySQLdb.escape_string(string)
+
     result = string.replace("'","''")
-    # adressiert das backslash problem
-    # möglicherweise noch probleme mit einem String + backslash am Ende
     result = result.replace('\\',' ')
     return result
 
@@ -50,10 +37,10 @@ def writeLog(string):
         myfile.write(string + "\n")
 
 def main():
-    db = connectToDb()
+    db = dbhelper.connectToDb('mysql')
     # you must create a Cursor object. It will let you execute all the queries you need
     cur = db.cursor()
-    start = 1413581400
+    start = 1402478743
     while 1:
         i = 0
         j = 0
@@ -62,7 +49,7 @@ def main():
             try:
                 submissions = getSubmissions(start)
             except Exception as e:
-                writeLog("ERROR:" + str(e))
+                writeLog("Fehler bei den Submissions: " + str(e))
                 time.sleep(4)
             else:
                 break
@@ -83,15 +70,13 @@ def main():
                     # problem ist die Struktur von reddit, was hier umgangen wird, indem alles platt geklopft wird
                     flat_comments = praw.helpers.flatten_tree(s.comments)
                 except Exception as e:
-                    writeLog("ERROR:" + str(e))
+                    writeLog("Fehler bei den Kommentaren: " + str(e))
                     time.sleep(4)
                 else:
                     break
 
             for c in flat_comments:
                 j = j + 1
-                #nochmal nachlesen: Problem "morecomments"
-                #if isinstance(c, praw.objects.Comment):
                 date = datetime.datetime.utcfromtimestamp(c.created_utc).strftime('%Y-%m-%d %H:%M:%S')
                 #print(str(c.created_utc), date, str(c.id))
                 #print ("---", str(c.ups), c.body.encode('ascii', 'ignore'))
