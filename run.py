e261be936a48439a6e6399ef8004ca9480581454
gregor <gpribb@gmail.com>
2015-04-17 20:39:20 +0200

    added sentiment analysis

--- run.py
+++ run.py
@@ -6,11 +6,14 @@ import praw
 import datetime
 import time
 import sys
+sys.path.insert(0, '/home/gregor/reddit/senti')
 import MySQLdb
 import dbhelper
+import sentistrength
+import re
 
 def addToDatabase(db, cur, created, date, link, title, body, ups, proc_on, pos, neg, score):
-    sql = "INSERT INTO `submissions`(`id`, `timestamp`, `date`, `link`, `title`, `body`, `upvotes`, `processed_on`, `eng_pos`, `eng_neg`, `score`) \
+    sql = "INSERT INTO `submissions_copy`(`id`, `timestamp`, `date`, `link`, `title`, `body`, `upvotes`, `processed_on`, `eng_pos`, `eng_neg`, `score`) \
     VALUES (NULL, '%s', '%s', '%s', '%s', '%s', '%i', '%s', '%i', '%i', '%i' )" % \
     (created, date, link, title, body, ups, proc_on, pos, neg, score)
     cur.execute(sql)
@@ -20,7 +23,7 @@ def getSubmissions(start):
     r = praw.Reddit(user_agent='get_me_reddit')
     # pro halben Tag (43200sec) eine Abfrage - quelle: http://www.reddit.com/r/redditdev/comments/2j6inf/how_to_download_submissions_of_a_specified_period/
     time_search_param = 'timestamp:' + str(start - 43200) + '..' + str(start)
-    writeLog(str(datetime.now()) + ":    von " + datetime.datetime.utcfromtimestamp(start-43200).strftime('%Y-%m-%d %H:%M:%S') + " bis " + datetime.datetime.utcfromtimestamp(start).strftime('%Y-%m-%d %H:%M:%S'))
+    writeLog(str(datetime.datetime.now()) + ":    von " + datetime.datetime.utcfromtimestamp(start-43200).strftime('%Y-%m-%d %H:%M:%S') + " bis " + datetime.datetime.utcfromtimestamp(start).strftime('%Y-%m-%d %H:%M:%S'))
     submissions = r.search(time_search_param, sort='new', subreddit='bitcoin', syntax='cloudsearch', limit=1000)
     return submissions
 
@@ -32,6 +35,10 @@ def escapeSqlProblems(string):
     result = result.replace('\\',' ')
     return result
 
+def escapeSentiProblems(string):
+    result = re.sub(r"[^A-Za-z]+", ' ', string)
+    return result
+
 def writeLog(string):
     with open("log", "a") as myfile:
         myfile.write(string + "\n")
@@ -40,7 +47,7 @@ def main():
     db = dbhelper.connectToDb('mysql')
     # you must create a Cursor object. It will let you execute all the queries you need
     cur = db.cursor()
-    start = 1402478743
+    start = 1422374551
     while 1:
         i = 0
         j = 0
@@ -55,12 +62,15 @@ def main():
                 break
 
         for s in submissions:
+            sentiment = sentistrength.senti(escapeSentiProblems(s.selftext))
+            pos = int(sentiment[:1])
+            neg = int(sentiment[3:4])
             i = i + 1
             #print(datetime.datetime.now(), "-------------------------")
             date = datetime.datetime.utcfromtimestamp(s.created_utc).strftime('%Y-%m-%d %H:%M:%S')
             #print (str(s.created_utc), date, str(s.id))
             #print (str(s.ups), "-", s.title.encode('ascii', 'ignore'))
-            addToDatabase(db,cur,int(s.created_utc),date,s.permalink,escapeSqlProblems(s.title),escapeSqlProblems(s.selftext),s.ups,'',0,0,0)
+            addToDatabase(db,cur,int(s.created_utc),date,s.permalink,escapeSqlProblems(s.title),escapeSqlProblems(s.selftext),s.ups,'',pos,neg,pos-neg)
 
             # probier 10x reddit zu erreichen
             for attempt in range(10):
@@ -77,10 +87,13 @@ def main():
 
             for c in flat_comments:
                 j = j + 1
+                sentiment = sentistrength.senti(escapeSentiProblems(c.body))
+                pos = int(sentiment[:1])
+                neg = int(sentiment[3:4])
                 date = datetime.datetime.utcfromtimestamp(c.created_utc).strftime('%Y-%m-%d %H:%M:%S')
                 #print(str(c.created_utc), date, str(c.id))
                 #print ("---", str(c.ups), c.body.encode('ascii', 'ignore'))
-                addToDatabase(db,cur,int(c.created_utc),date,c.permalink,'',escapeSqlProblems(c.body),c.ups,'',0,0,0)
+                addToDatabase(db,cur,int(c.created_utc),date,c.permalink,'',escapeSqlProblems(c.body),c.ups,'',pos,neg,pos-neg)
         # rewind please! half a day = 43200sec
         start = start - 43201
         writeLog("number of submissions: " + str(i))
